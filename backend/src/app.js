637622523155b3f29efbbf4abee0b58a85f0eada
OsmaroB <osma204bonillamestizo@hotmail.com>
2020-07-18 11:28:11 -0600

    Estructuracion del backend conexion db y llamado al servidor principal en el puerto 4000

---
+++ backend/src/app.js
@@ -0,0 +1,12 @@
+const express = require('express');//Codigo de ES5
+const app = express();
+
+
+//Setting 
+app.set('port', 4000);
+//Middlewares
+
+//routes
+
+//Aca solamente definimos el servidor
+module.exports = app;
