75449c296b02bcf4ee7a95320cfe2a109be34472
OsmaroB <osma204bonillamestizo@hotmail.com>
2020-07-22 07:58:54 -0600

    Delete Notes terminado solamente falta el UPDATE Notes

--- frontend/src/components/NoteList.js
+++ frontend/src/components/NoteList.js
@@ -1,10 +1,54 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import {format} from 'timeago.js'
 
 export default class NoteList extends Component {
+   
+    state ={
+        notes: []
+    }
+    
+    async componentDidMount(){
+        this.getNotes();
+    }
+
+    async getNotes(){
+        const res = await axios.get('http://localhost:4000/api/notes');
+        this.setState({
+            notes: res.data
+        })
+    }
+
+    deleteNote = async (id)=>{
+        await axios.delete('http://localhost:4000/api/notes/'+id);
+        this.getNotes();
+    }
+
     render() {
         return (
-            <div>
-                Notes List
+            <div className="row">
+                {
+                    this.state.notes.map( note =>(
+                        <div className="col-md-4 p-4" key={note._id}>
+                            <div className="card" >
+                                <div className="card-header">
+                                    <h3>{note.title}</h3>
+                                </div>
+                                <div className="card-body">
+                                    <p>{note.content}</p>
+                                    <p>Author: {note.author}</p>
+                                    <p>{format(note.date)}</p>
+                                </div>
+                                <div className="card-footer">
+                                    <button 
+                                    className="btn btn-danger"
+                                    onClick={ ()=> this.deleteNote(note._id)}
+                                    >Delete</button>
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                }
             </div>
         )
     }
